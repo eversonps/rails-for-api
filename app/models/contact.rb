699fa9d165db6891e359ac367a65b6fd7d29b2a4
Everson <eversonpereiraofc@gmail.com>
2022-06-02 15:20:25 -0300

    create CRUDs, render json and belongs_to

---
+++ app/models/contact.rb
@@ -0,0 +1,18 @@
+class Contact < ApplicationRecord
+  belongs_to :kind, optional: true
+
+  # def author 
+  #   "Jackson Pires"
+  # end
+
+  # def kind_description 
+  #   self.kind.description
+  # end
+
+  # def as_json(options={})
+  #   super(
+  #     methods: [:kind_description, :author],
+  #     root: true,
+  #   )
+  # end
+end
